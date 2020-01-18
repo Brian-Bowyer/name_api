873787a0eb4db15c506431f570c1865ccf2dab07
Brian-Bowyer <bobsnake64@gmail.com>
2020-01-17 22:23:43 -0600

    introduce ANY; make params optional

--- app/main.py
+++ app/main.py
@@ -3,13 +3,18 @@ from random import choice
 
 app = FastAPI()
 
+GENDERS = ['male', 'female', 'nonbinary']
 NAMES = {
     'male': ['Bob', 'Joe', 'Sam'],
     'female': ['Mary', 'Suzy', 'Abigail'],
     'nonbinary': ['Skylar', 'Robin'],
 }
+ANY = [element for gender in GENDERS for element in NAMES[gender]]
+
 
 async def generate_name(gender: str):
+    if gender == 'any':
+        return choice(ANY)
     return choice(NAMES[gender])
 
 @app.get("/")
@@ -21,7 +26,7 @@ async def ping():
     return {'success': 1}
 
 @app.get("/names")
-async def generate_names(gender: str, count: int):
+async def generate_names(gender: str = 'any', count: int = 1):
     names = []
     for i in range(count):
         names.append(await generate_name(gender=gender))
